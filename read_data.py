8bee12a193ed31701b9971f02b16ba89702850cc
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-02-08 23:12:56 +0500

    read

--- read_data.py
+++ read_data.py
@@ -11,4 +11,9 @@ def fromJson(file_path: str)->dict:
         dict: Dictionary containing the data of the json file.
     
     """
-    return
+
+
+    data = json.loads(f)
+    return data
+f=open("data/result.json", encoding="utf8").read()
+# print(fromJson('f'))
